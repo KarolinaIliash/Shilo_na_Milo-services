aa9e7b6f84b3d8977e1f8ce74345af34a2c3d7e1
KarolinaIliash <karolina.iliash@gmail.com>
2018-12-04 11:43:15 +0200

    Added exception catching in AddUpdateController.

--- project/spring_boot_app/src/main/java/Main/Constants/Codes.java
+++ project/spring_boot_app/src/main/java/Main/Constants/Codes.java
@@ -5,4 +5,5 @@ public class Codes {
     public static final Integer BadRequest = 400;
     public static final Integer Forbidden = 403;
     public static final Integer Anauthorized = 401;
+    public static final Integer InternalServerError = 500;
 }
--- project/spring_boot_app/src/main/java/Main/Controller/AddUpdateController.java
+++ project/spring_boot_app/src/main/java/Main/Controller/AddUpdateController.java
@@ -23,77 +23,111 @@ public class AddUpdateController {
 
     @PostMapping("/add_service")
     public Answer<Service> addNewService(@RequestBody Service newService) {
-        Date date = new Date();
-        newService.setLast_modified(date);
+        try {
+            Date date = new Date();
+            newService.setLast_modified(date);
+            newService.setMark_amount(0);
+            newService.setMark(0d);
+            if (newService.getPrice() == null) {
+                newService.setPrice(0d);
+            }
+            newService = serviceRepository.save(newService);
 
-        newService = serviceRepository.save(newService);
+            UtilFuncs.UpdateDoc(newService);
 
-        UtilFuncs.UpdateDoc(newService);
-
-        return new Answer<>(Codes.OK, newService);
+            return new Answer<>(Codes.OK, newService);
+        }
+        catch (Exception e){
+            Service errorService = new Service();
+            errorService.setName("error");
+            errorService.setDescription(e.getMessage());
+            return new Answer<>(Codes.InternalServerError, errorService);
+        }
     }
 
     @PostMapping("/update_service")
     public Answer<Service> replaceService(@RequestBody Service newService) {
+        try {
+            Service result_service =
+                    serviceRepository.findById(newService.getId())
+                            .map(service -> {
+                                service.setName(newService.getName());
+                                service.setDescription(newService.getDescription());
+                                service.setCategory(newService.getCategory());
+                                service.setMark(newService.getMark());
+                                service.setMark_amount(newService.getMark_amount());
+                                service.setPrice(newService.getPrice());
+                                service.setLast_modified(new Date());
+                                return serviceRepository.save(service);
+                            })
+                            .orElseGet(() -> {
+                                newService.setLast_modified(new Date());
+                                return serviceRepository.save(newService);
+                            });
 
-         Service result_service =
-                serviceRepository.findById(newService.getId())
-                .map(service -> {
-                    service.setName(newService.getName());
-                    service.setDescription(newService.getDescription());
-                    service.setCategory(newService.getCategory());
-                    service.setMark(newService.getMark());
-                    service.setMark_amount(newService.getMark_amount());
-                    service.setPrice(newService.getPrice());
-                    service.setLast_modified(new Date());
-                    return serviceRepository.save(service);
-                })
-                .orElseGet(() -> {
-                    newService.setLast_modified(new Date());
-                    return serviceRepository.save(newService);
-                });
+            UtilFuncs.UpdateDoc(result_service);
 
-        UtilFuncs.UpdateDoc(result_service);
-
-        return new Answer<>(Codes.OK, result_service);
+            return new Answer<>(Codes.OK, result_service);
+        }
+        catch (Exception e){
+            Service errorService = new Service();
+            errorService.setName("error");
+            errorService.setDescription(e.getMessage());
+            return new Answer<>(Codes.InternalServerError, errorService);
+        }
     }
 
     @PostMapping("/add_mark/{id}/{mark}")
     public Answer<Service> addMark(@PathVariable Integer id, @PathVariable double mark) {
-        Service result_service = serviceRepository.findById(id)
-                .map(service -> {
-                    service.setMark_amount(service.getMark_amount() + 1);
-                    service.setMark((mark + service.getMark()) / service.getMark_amount());
-                    service.setLast_modified(new Date());
-                    return serviceRepository.save(service);
-                })
-                .orElseGet(()-> {return null;});
+        try {
+            Service result_service = serviceRepository.findById(id)
+                    .map(service -> {
+                        service.setMark_amount(service.getMark_amount() + 1);
+                        service.setMark((mark + service.getMark()) / service.getMark_amount());
+                        service.setLast_modified(new Date());
+                        return serviceRepository.save(service);
+                    })
+                    .orElseGet(() -> {
+                        return null;
+                    });
 
-        UtilFuncs.UpdateDoc(result_service);
+            UtilFuncs.UpdateDoc(result_service);
 
-        return new Answer<>(Codes.OK, result_service);
+            return new Answer<>(Codes.OK, result_service);
+        }
+        catch (Exception e){
+            Service errorService = new Service();
+            errorService.setName("error");
+            errorService.setDescription(e.getMessage());
+            return new Answer<>(Codes.InternalServerError, errorService);
+        }
     }
 
     @PutMapping("/delete/{id}")
     public EmptyAnswer delete(@PathVariable Integer id) {
-        serviceRepository.deleteById(id);
-
-        boolean ok = true;
-
-        HttpSolrClient client = UtilFuncs.getSolrClient();
         try {
-            client.deleteById(id.toString());
-            client.commit();
-        } catch (SolrServerException e) {
-            e.printStackTrace();
-            ok = false;
-        } catch (IOException e) {
-            e.printStackTrace();
-            ok = false;
-        }
-        //TODO implement bad codes support
-        //Integer code = ok ? Codes.OK : Codes.
+            serviceRepository.deleteById(id);
 
-        return new EmptyAnswer(Codes.OK);
+            boolean ok = true;
+
+            HttpSolrClient client = UtilFuncs.getSolrClient();
+            try {
+                client.deleteById(id.toString());
+                client.commit();
+            } catch (SolrServerException e) {
+                e.printStackTrace();
+                ok = false;
+            } catch (IOException e) {
+                e.printStackTrace();
+                ok = false;
+            }
+            //TODO implement bad codes support
+            //Integer code = ok ? Codes.OK : Codes.
+
+            return new EmptyAnswer(Codes.OK);
+        }
+        catch (Exception e){
+            return new EmptyAnswer(Codes.InternalServerError);
+        }
     }
 }
